c1ae26134c344ec20b7ab03a99d5c0abffcb3dfc
(Itz_Me) <vedanimefan@gmail.com>
2025-08-22 20:05:55 +0530

    Add 11_2025_4.py to general category

---
+++ examples/general/11_2025_4.py
@@ -0,0 +1,9 @@
+#Program to input 5 different Subject Marks and Calculate Total and Percent
+#and display Grade According to Percent
+# Percent                 Grade
+#if Percent>75            Distn.
+#if Percent>=60 and <75   First
+#if Percent>=45 and <60   Second
+#if Percent>=33 and <45   Third
+# otherwise               Failed 
+      
